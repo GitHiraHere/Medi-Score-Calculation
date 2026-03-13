320f0232bb7ffd5ea830d0e5d9ff5a10e2a71119
Hira <HiraShahid2006@gmail.com>
2026-03-13 20:08:19 +0000

    Added SpO2 calculation and began bug testing

--- src/Medi_Score.java
+++ src/Medi_Score.java
@@ -1,7 +1,12 @@
 public class Medi_Score {
+    static void main(String[] args) {
+        int score = Medi_Score.takeScore(); // Have to lead with medi_score has takeScore is defined in the class not under main
+
+        System.out.println("Final score: " + score);
+    }
     public static int takeScore(){ // the main function where the patients score will be recorded
         int score = 0; // where the final score will be stored
-        Observations patientObs = new Observations(0, 0, 12, 88, 36.1);
+        Observations patientObs = new Observations(2, 0, 17, 95, 37.1);
 
         if(patientObs.getairOrOxygen() == 0){
             System.out.println("The patient is not on oxygen!");
@@ -24,15 +29,13 @@ public class Medi_Score {
         if(patientObs.getRespirationRate() <= 8){
             System.out.println("Respiratory rate concerningly low, 3 points added.");
             score += 3;
-        } else if(patientObs.getRespirationRate() == 9 || patientObs.getRespirationRate() == 10 ||
-                patientObs.getRespirationRate() == 11){
-            System.out.println("Respiratory rate is mildly low, no points added.");
+        } else if(patientObs.getRespirationRate() >= 9 && patientObs.getRespirationRate() <= 11){
+            System.out.println("Respiratory rate is low, no points added.");
             score += 1;
         } else if(patientObs.getRespirationRate() >= 12 && patientObs.getRespirationRate() <= 20){
             System.out.println("Respiratory rate is good, 0 points have been added");
             // Score 0
-        } else if(patientObs.getRespirationRate() == 21 || patientObs.getRespirationRate() == 22 ||
-                patientObs.getRespirationRate() == 23 ||  patientObs.getRespirationRate() ==24){
+        } else if(patientObs.getRespirationRate() >= 21 && patientObs.getRespirationRate() <= 24){
             System.out.println("Respiratory rate is concerning, 2 points have been added");
             score += 2;
         } else if(patientObs.getRespirationRate() >= 25){
@@ -41,17 +44,43 @@ public class Medi_Score {
         }
 
         //SpO2 will need to measure if someone is on oxygen or not as well as their spo2 level to determine score
+        if(patientObs.getSpO2() <= 83){
+            System.out.println("SpO2: Added 3 points to the score.");
+                score += 3;
+        } else if (patientObs.getSpO2() >= 84 && patientObs.getSpO2() <= 85){
+            System.out.println("SpO2: Added 2 points to the score.");
+            score += 2;
+        } else if (patientObs.getSpO2() >= 86 && patientObs.getSpO2() <= 87){
+            System.out.println("SpO2: Added 1 point to the score.");
+            score += 1;
+        }else if (patientObs.getSpO2() >= 88 && patientObs.getSpO2() <= 92) {
+            System.out.println("SpO2: 0 points have been added.");
+        }else if (patientObs.getairOrOxygen() == 0 && patientObs.getSpO2() >= 93) {
+            System.out.println("SpO2: 0 points have been added.");
+        }else if (patientObs.getairOrOxygen() == 2 && patientObs.getSpO2() >= 93 && patientObs.getTemp() <= 94) {
+            System.out.println("SpO2: 1 point has been added. testing");
+            score += 1;
+        }else if (patientObs.getairOrOxygen() == 2 && patientObs.getSpO2() >= 95 && patientObs.getTemp() <= 96) {
+            System.out.println("SpO2: 2 points have been added.");
+            score += 2;
+        }else if(patientObs.getairOrOxygen() == 2 && patientObs.getSpO2() >= 97){
+            System.out.println("SpO2: 3 points have been added.");
+            score += 3;
+        }
 
         if(patientObs.getTemp() <= 35.0){
-            System.out.println("Added 3 points to the score.");
+            System.out.println("Temp is concerning, added 3 points to the score.");
             score += 3;
-        } else if(patientObs.getTemp() <= 36.0 || patientObs.getTemp() <= 37.0 || patientObs.getTemp() <= 38.0){
-            // Score 0, nothing added
+        } else if(patientObs.getTemp() >= 35.1 && patientObs.getTemp() <= 36.0){
+            System.out.println("Temp is mildly concerning, added 1 point to the score.");
+            score += 1;
+        } else if(patientObs.getTemp() >= 36.1 && patientObs.getTemp() <= 38.0){
+            System.out.println("Temp is fine, 0 points have been added");
         } else if(patientObs.getTemp() <= 39.0) {
-            System.out.println("Added 1 point to the score.");
+            System.out.println("Temp is mildly concerning, added 1 point to the score.");
             score += 1;
         } else if(patientObs.getTemp() >= 39.1){
-            System.out.println("Added 2 points to the score.");
+            System.out.println("Temp is concerning, added 2 points to the score.");
             score += 2;
         }
         return score;
